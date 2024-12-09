169e77ef027f32abf87eaf1bcd403e6a2b8907f0
Mariia Tararaeva <mariatararaeva@icloud.com>
2024-12-09 15:32:03 +0100

    solved lab

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
+
 class SortedList {
-  constructor() {}
+  
+  constructor() {
+    this.items = [];
+    this.length = 0;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+  this.items.sort((a, b) => a -b);
+  this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if(pos< 1 || pos > this.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[this.length -1];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");    
+    }
+    return this.items[this.length -1]
+  }
 
-  sum() {}
+  min() {
+if (this.length === 0) {
+  throw new Error("EmptySortedList");  
+}
+return this.items[0];
+}
+  sum() {
+    return this.items.reduce((a, b) => a+b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");  
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
+
+//The task here is to create a class -
+//that maintains a sorted list of numbers in ascending order.
